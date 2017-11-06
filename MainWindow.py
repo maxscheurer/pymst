d50a482741ea0285fc2d8df2c159d23b56bc4bcd
maxscheurer <max.scheurer@me.com>
2017-11-06 13:53:52 +0100

    basic functionality

--- MainWindow.py
+++ MainWindow.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import warnings
 import copy
+import sip
 
 import PyQt5.QtCore as QtCore
 from PyQt5.QtCore import QRect, pyqtSlot, QObject
@@ -8,8 +9,73 @@ from PyQt5.QtWidgets import (QMainWindow, QTabWidget, QLabel, QDialog, QProgress
                              QApplication, QGridLayout, QFileDialog, QColorDialog, QWidget)
 from PyQt5.Qt import Qt, QColor
 import numpy as np
+from scipy.optimize import curve_fit
 
 import MainWindow_gui
+from Plotters import SimplePlotter
+
+def read_file(fname):
+    x = []
+    y = []
+    with open(fname) as fin:
+        dataflag = 0
+        # 0: not recording, 1: start of dataset, 2: inside dataset
+        for line in fin:
+            l = line.strip()
+            if "Data Analysis [Thermophoresis With Temperature Jump]" in l:
+                dataflag = 1
+                continue
+            if dataflag:
+                if l == "":
+                    continue
+                elif "Average and Error Bars" in l:
+                    dataflag = 0
+                    break
+                else:
+                    # print(l.split("\t"))
+                    xl, yl = l.split("\t")
+                    x.append(xl)
+                    y.append(yl)
+    return [np.array(x, dtype=float), np.array(y, dtype=float)]
+
+
+class MST_CurveFit(object):
+    """docstring for MST_CurveFit."""
+
+    def __init__(self, fname, protein_conc):
+        super(MST_CurveFit, self).__init__()
+        self.read_in = read_file(fname)
+        self.lig_conc = self.read_in[0] # ligand concentrations (unlabeled substance)
+        self.ratios = self.read_in[1] # depletion ratio
+        print(self.lig_conc)
+        print(self.ratios)
+        self.protein_conc = float(protein_conc)
+
+    def fluo_func(self, a, kd, fnb, fnab):
+        b = self.protein_conc
+        rad = np.power(a+b+kd,2) - 4*a*b
+        ab = (a+b+kd-np.sqrt(rad))/2
+        b_l = b-ab
+        return (ab/b)*fnab + (b_l/b)*fnb
+
+    def fit_mst_curve(self, name):
+        x_min = 10
+        x_max_offset = 1e6
+        dx = 100
+        x = self.lig_conc
+        y = self.ratios
+        # run fit
+        self.popt, self.pcov = curve_fit(self.fluo_func, x, y)
+        # prepare plot
+        self.kd = self.popt[0]
+        residuals = y - self.fluo_func(x, *self.popt)
+        ss_res = np.sum(residuals**2)
+        ss_tot = np.sum((y - np.mean(y))**2)
+        self.r_squared = 1 - (ss_res / ss_tot)
+        print(self.r_squared)
+
+        self.x_lin = np.arange(x_min, np.max(x) + x_max_offset, dx)
+
 
 class MainWindow(QMainWindow, MainWindow_gui.Ui_MainWindow, QObject):
     """PyContact Application Main Window with timeline."""
@@ -24,6 +90,14 @@ class MainWindow(QMainWindow, MainWindow_gui.Ui_MainWindow, QObject):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
         self.actionLoad.triggered.connect(self.loadFile)
+        self.fitButton.clicked.connect(self.loadFile)
+        self.grid = QGridLayout()
+        self.plotWidget.setLayout(self.grid)
+        self.plot = SimplePlotter()
+        self.grid.addWidget(self.plot)
+        self.kdField = QLabel("Kd = ---")
+        self.gridLayout_2.addWidget(self.kdField, 3, 1, 1, 1)
+
 
 
     def loadFile(self):
@@ -35,4 +109,10 @@ class MainWindow(QMainWindow, MainWindow_gui.Ui_MainWindow, QObject):
             break
         if importfile == "" or len(fnames) == 0:
             return
-        
+        mst = MST_CurveFit(importfile, self.concField.text())
+        mst.fit_mst_curve('test')
+        sip.delete(self.plot)
+        self.plot = SimplePlotter()
+        self.grid.addWidget(self.plot)
+        self.plot.plot(mst.x_lin, mst.lig_conc, mst.ratios, mst.popt, mst.fluo_func)
+        self.kdField.setText("Kd = %.2f nM" % mst.kd)
---
+++ Plotters.py
@@ -0,0 +1,71 @@
+import matplotlib
+from PyQt5.QtWidgets import QSizePolicy
+from matplotlib import pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg \
+    as FigureCanvas
+from matplotlib.figure import Figure
+plt.style.use('ggplot')
+
+font = {'family' : 'normal',
+        'weight' : 'normal',
+        'size'   : 14}
+
+matplotlib.rc('font', **font)
+
+
+class MplPlotter(FigureCanvas):
+    """Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
+
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        self.fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = self.fig.add_subplot(111)
+        # We want the axes cleared every time plot() is called
+        # self.axes.hold(False)
+
+        self.compute_initial_figure()
+
+        FigureCanvas.__init__(self, self.fig)
+        self.setParent(parent)
+
+        FigureCanvas.setSizePolicy(self,
+                                   QSizePolicy.Expanding,
+                                   QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+
+    def compute_initial_figure(self):
+        pass
+
+
+class SimplePlotter(MplPlotter):
+    """Simple plotter, used in the SASA view."""
+    def onpick(self, event, line):
+        print("EVENT!")
+        if event.artist!=line: return True
+
+        N = len(event.ind)
+        if not N: return True
+
+
+        # figi = plt.figure()
+        for subplotnum, dataind in enumerate(event.ind):
+            print(subplotnum, dataind)
+        return True
+
+    def plot(self, x_lin, x_raw, y_raw, popt, func):
+        line, = self.axes.semilogx(x_raw, y_raw, "o", picker=5)
+
+        line2, = self.axes.semilogx(x_lin, func(x_lin, *popt), "-", label=r"$K_d$ = %.2f nM" % popt[0])
+        # plt.semilogx(x, y, "o")
+
+        # plt.ylim([870,940])
+        self.fig.canvas.mpl_connect('pick_event', lambda event: self.onpick(event, line))
+        # self.axes.plot(x, y)
+        self.axes.set_xlabel("concentration [nM]")
+        self.axes.set_ylabel("fluo. ratio")
+        # self.axes.xaxis.set_label_position('top')
+
+    def saveFigure(self, path, outputFormat):
+        self.fig.savefig(path + "." + outputFormat, format=outputFormat)
+
+    def clearFigure(self):
+        self.fig.clf()
